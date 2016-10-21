7f2c7e259439660765b8d5e2377773cd406df224
authorbeard <msnolan@gmail.com>
2016-10-21 10:59:04 -0400

    rewrote hamwater listener to correct post body display; added switch statement and jquery selectors

--- js/power.js
+++ js/power.js
@@ -5,6 +5,8 @@ $(function() {
     $('.pagination .active a').click(function() {
         return false;
     });
+
+    centerLanding();
 });
 
 
@@ -12,46 +14,21 @@ $(function() {
 =            WINDOW IS LOADED            =
 ========================================*/
 $(window).load(function() {
-  var aside = document.querySelector('aside')
-  var titleBar = document.querySelectorAll('.title-group, .post-title')
-  var navBar = document.querySelector('nav')
-  var topperHeight = document.querySelector('.topper').clientHeight;
-  var pdHeight = document.querySelector('.post-details').clientHeight;
-  var containerPost = document.querySelector('.container.post');
-  function setPostTop(){
-    console.log("topperHeight:" + topperHeight);
-    console.log("pdHeight:" + pdHeight);
-    containerPost.style.cssText = "top: " + (topperHeight-pdHeight)*-1 + "px;";
-  };
-  setPostTop()
 
+  var titleBar = $('.title-group, .post-title')
+  switch ($('body').attr('class')){   
+    case 'blog':
+      $('.title-group, .post-title').children('aside').remove() 
+      break;
+    case 'category-blog':
+      setPostTop()
+      break;
 
-  var setHamwater=function(){
-    // debugger;
-    titleBar[0].style.cssText="top: -37px; position: fixed; width:100%; z-index:1100"
-    titleBar[1].style.cssText="margin-top: 100px;"
-    navBar.style.cssText="opacity: 0; transition: ease-in-out;"
-  } 
+  }
 
-  window.setTimeout(function(){ 
-    if (document.body.className==' category-blog'){
-      titleBar[0].removeChild(aside)
-    }
-  })
+  // setPostTop()
+  // setHamwater()
 
-  document.addEventListener('scroll', function(){
-    if (document.body.className ==' category-blog'){
-      if (document.body.scrollTop >= 25){
-        setHamwater()
-      }
-      if (document.body.scrollTop === 0){
-        titleBar.forEach(function(div){ 
-          div.style.cssText=""
-        })
-        navBar.style.cssText=""
-      }
-    }
-  })
 });
 
 
@@ -59,6 +36,7 @@ $(window).load(function() {
 =            WINDOW IS RESIZED            =
 =========================================*/
 $(window).resize(function() {
+  centerLanding()
 
 });
 
@@ -68,7 +46,47 @@ $(window).resize(function() {
 ==========================================*/
 $(window).scroll(function() {
 
+  if ($('body').hasClass('category-blog')){
+    if (document.body.scrollTop >= 25){
+      setHamwater()
+    }
+    if (document.body.scrollTop === 0){
+      titleBar.forEach(function(div){ 
+        div.style.cssText=""
+      })
+      navBar.style.cssText=""
+    }
+  }
+
 });
 
+
+function centerLanding (){
+  if ($('body').hasClass('landing')){
+      var mainTop = Math.floor(($(window).height() - $('.landing-nav').height()) / 2)
+      $('main').css('top', mainTop)
+    }
+}
+
+var setPostTop = function(){
+debugger;
+  var navBar = $('nav')
+  var topperHeight = $('.topper').height();
+  var pdHeight = $('.post-details').height();
+  var containerPost = $('.container.post');
+
+
+
+  console.log("topperHeight:" + topperHeight);
+  console.log("pdHeight:" + pdHeight);
+  containerPost.css("top", (pdHeight-topperHeight));
+};
+
+var setHamwater=function(){
+
+  // titleBar[0].style.cssText="top: -37px; position: fixed; width:100%; z-index:1100"
+  // titleBar[1].style.cssText="margin-top: 100px;"
+  // navBar.style.cssText="opacity: 0; transition: ease-in-out;"
+} 
 
 
